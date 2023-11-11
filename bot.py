22bd76f28b55ee25dbadca76375fdd407960e297
mkrzeszewski <64884451+mkrzeszewski@users.noreply.github.com>
2023-11-11 18:56:11 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ def runDiscordBot():
 
     @client.event
     async def on_message(message):
-        if message.author = client.user:
+        if message.author == client.user:
             return
     print(message.content)
 
