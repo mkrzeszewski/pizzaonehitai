ddba26a2af5496fd0cf5aeb7e16209e39bccd7de
mkrzeszewski <64884451+mkrzeszewski@users.noreply.github.com>
2023-11-13 02:01:43 +0100

    add weather check in lodz every hour

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import discord
 import responses
+import requests
+from discord.ext import tasks
 
 async def sendMessage(message, user_message, is_private):
     try:
@@ -27,6 +29,7 @@ def runDiscordBot():
     @client.event
     async def on_ready():
         print(f'{client.user} is now running!')
+        sendWeather.start()
 
     @client.event
     async def on_message(message):
@@ -37,5 +40,41 @@ def runDiscordBot():
         if userMessage[0] == '!':
             userMessage = userMessage[1:]
             await sendMessage(message, userMessage, is_private=False)
+
+    index = 0
+
+    @tasks.loop(hours = 1.0)
+    async def sendWeather():
+        channel = client.get_channel(1172911430601822238)
+        #await channel.send('test')
+        
+        BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+        CITY = "lodz"
+        API_KEY = "1f4b8c61bd3abc5071c8b2e823e465cb"
+        # upadting the URL
+        URL = BASE_URL + "q=" + CITY + "&appid=" + API_KEY
+        # HTTP request
+        response = requests.get(URL)
+        if response.status_code == 200:
+            # getting data in the json format
+            data = response.json()
+            # getting the main dict block
+            main = data['main']
+            # getting temperature
+            temperature = main['temp']
+            # getting the humidity
+            humidity = main['humidity']
+            # getting the pressure
+            pressure = main['pressure']
+            # weather report
+            report = data['weather']
+            print(f"{CITY:-^30}")
+            print(f"Temperature: {temperature}")
+            print(f"Humidity: {humidity}")
+            print(f"Pressure: {pressure}")
+            print(f"Weather Report: {report[0]['description']}")
+            await channel.send("temperaturka w lodzi: "+temperature+". L4D2 juz blisko!")
+        else:
+            print(response.status_code)
 
     client.run(TOKEN)
--- main.py
+++ main.py
@@ -3,5 +3,3 @@ import bot
 if __name__ == '__main__':
     #run the bot
     bot.runDiscordBot()
-    channel = bot.get_channel(1172911430601822238)
-
