8d363a38738f1b331b20d149226c2d8a40bb14bb
mkrzeszewski <64884451+mkrzeszewski@users.noreply.github.com>
2025-02-25 02:31:06 +0100

    tidy ai code

--- plugins/ai.py
+++ plugins/ai.py
@@ -6,15 +6,6 @@ client = genai.Client(api_key=GOOGLE_AI_API_KEY)
 MAX_LENGTH = 2000
 
 def getResponse(message):
-
     message += "\n Also ensure the response is rather short, no longer than 1000 characters"
     response = client.models.generate_content(model="gemini-2.0-flash", contents=message)
-    
-    return response.text[:MAX_LENGTH] 
-
-def getResponse(message):
-
-    message += "\n Also ensure the response is rather short, no longer than 1000 characters"
-    response = client.models.generate_content(model="gemini-2.0-flash", contents=message)
-    
     return response.text[:MAX_LENGTH] 
