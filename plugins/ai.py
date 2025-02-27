2cadc2f696ded825c50ea4ba6a7647239824eb1c
mkrzeszewski <64884451+mkrzeszewski@users.noreply.github.com>
2025-02-27 21:47:53 +0100

    add history and initial instructions

--- plugins/ai.py
+++ plugins/ai.py
@@ -1,10 +1,15 @@
 from google import genai
 from os import environ
+import plugins.pizzadatabase as db
 
 GOOGLE_AI_API_KEY = environ["GOOGLE_GEMINI_API_KEY"]
 client = genai.Client(api_key=GOOGLE_AI_API_KEY)
 chat = client.chats.create(model="gemini-2.0-flash")
-MAX_LENGTH = 2000
+initialInstructions = db.retrieveAllAIInstructions()
+for record in initialInstructions:
+    chat.send_message(record['instruction'])
+
+MAX_LENGTH = 1500
 
 def chatWithAI(message):
     response = chat.send_message_stream(message)
