4bad915550d48f733ae2a5fff2e30aa56081da55
mkrzeszewski <64884451+mkrzeszewski@users.noreply.github.com>
2025-03-02 18:25:52 +0100

    modify heist to use db

--- plugins/heist.py
+++ plugins/heist.py
@@ -3,6 +3,7 @@ import plugins.ai as ai
 import plugins.pizzadatabase as db
 from datetime import datetime
 
+
 random.seed(datetime.now().timestamp())
 medium_chance = 80
 
@@ -12,19 +13,25 @@ circumstances = ["", " w bialy dzien", " pod oslona nocy", " w samo poludnie", "
 
 
 #returns level ["hard", "medium"] and heist name
-def generateHeistName():
+def generateHeist():
     heist_list = medium_target_list
     level = "medium"
     if random.randint(1, 100) < medium_chance:
         heist_list = hard_target_list
         level = "hard"
-    return level, "Napad na " + random.choice(heist_list) + random.choice(circumstances)
+    
+    initial_loot = random.randint(1000,10000)
+    initial_chance = random.randint(30,80)
+    heist_name = "Napad na " + random.choice(heist_list) + random.choice(circumstances)
+    db.initializeHeist(heist_name, initial_loot, initial_chance)
+    return level, heist_name, initial_loot, initial_chance
 
 def generateHeistIntro(heist_name):
     return ai.askAI("Wygeneruj zabawny, krotki wstep do napadu o nazwie:" + str(heist_name) + ", zachejaca uczestnikow gry tekstowej do przystapienia do zabawy. Nie informuj jak dolaczyc do napadu - ta czesc zostanie dodana przeze mnie.")
 
 #returns - intro, simulation, ending, json with results
-def heistGeneration(heist_name, members):
+def heistSimulation(heist_name, initial_loot, initial_chance):
+    members = list(db.retrieveHeistMembers())
     if members:
         if len(members) > 1:
             listOfCommands = []
@@ -33,12 +40,15 @@ def heistGeneration(heist_name, members):
             for member in members:
                 memberStr += str(member[0]) + ":" +str(member[1]) + ","
             listOfCommands.append("members: "+memberStr[:-1])
-            listOfCommands.append("chance: " + str(random.randint(30,80)) + "%")
-            listOfCommands.append("expected_loot: "+str(random.randint(1000,10000)))
+            listOfCommands.append("chance: " + str(initial_chance) + "%")
+            listOfCommands.append("expected_loot: "+str(initial_loot))
             acts = ai.generateHeist(listOfCommands).split("ROZDZIELNIK_ETAP")
             return acts[0], acts[1], acts[2], acts[3].strip().lstrip('```json\n').rstrip('```')
         else:
-            return "Za malo osob wzielo udzial w napadzie."
+            return "Za malo osob wzielo udzial w napadzie.", None, None, None
+    else:
+        return "Nikt nie wzial udzialu w napadzie.", None, None, None
 
-def finalizeHeist():
+def finalizeHeist(json_result):
+    db.moveCurrentHeistToHistory(json_result)
     return None
