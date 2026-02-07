91a9b94eed1aec674a069930fb8a90022c963679
mkrzeszewski <64884451+mkrzeszewski@users.noreply.github.com>
2026-02-07 13:24:41 +0100

    add import

--- plugins/theatres.py
+++ plugins/theatres.py
@@ -1,5 +1,12 @@
 import plugins.pizzadatabase as db
 import plugins.scrapers.scraper as scrapers
+from plugins.scrapers.gdynia import Gdynia
+from plugins.scrapers.iksoris import Iksoris
+from plugins.scrapers.poznan import Poznan
+from plugins.scrapers.roma import Roma
+from plugins.scrapers.scraper import Scraper
+from plugins.scrapers.variete import Variete
+from plugins.scrapers.wroclaw import Wroclaw
 from bs4 import BeautifulSoup
 import urllib3
 
@@ -63,49 +70,3 @@ def checkNewEvents():
                     print(msg)
     except Exception as e:
         print(e)
-# def getAllEventsIksoris(url):
-#     try:
-#         # Fetch the web content
-#         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36'}  # Some sites require user-agent
-#         response = requests.get(url, headers=headers, verify=False)
-#         response.raise_for_status()  # Raise an error for bad status codes
-#         # Parse the HTML content
-#         soup = BeautifulSoup(response.text, 'html.parser')
-#         # Find all <a> tags and extract href attributes
-#         events = []
-#         for h2_tag in soup.find_all('h2', ["h5","text-primary"]):
-#            events.append(h2_tag.get_text())
-#         return events
-#     except requests.exceptions.RequestException as e:
-#         print(f"Error fetching {url}: {e}")
-#         return []
-
-# def checkNewEvents():
-#     returnText = ""
-#     theatreEvents = []
-#     theatres = db.retrieveAllTheatres()
-#     for theatre in theatres:
-#         eventsOnSite = []
-#         newEvents = []
-#         if theatre['system'] == "iksoris":
-#             eventsOnSite = getAllEventsIksoris(theatre['url'])
-#         # elif theatre['system'] == "roma":
-#         #     x = 1
-#         # elif theatre['system'] == "xxx":
-#         #     x = 1
-
-#         eventsInDb = db.retrieveEventsFromTheatre(theatre['name'])
-#         for event in eventsInDb:
-#             if event['name'] not in eventsOnSite:
-#                 db.removeEvent('name',event['name'])
-        
-#         for event in eventsOnSite:
-#             if not db.retrieveEventFromTheatre(theatre['name'], event):
-#                 newEvents.append(event)
-#                 db.addEvent(event, theatre['name'])
-
-#         if len(newEvents) > 0:
-#             returnText += "Nowe wydarzenie na stronie " + str(theatre['name']) + ".\n"
-#             returnText += "\n - ".join(newEvents)
-#             theatreEvents.append([theatre, newEvents])
-#     return theatreEvents
