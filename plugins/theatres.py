fab476a4b52f31b6762ec27960d156bfe93652b2
mkrzeszewski <64884451+mkrzeszewski@users.noreply.github.com>
2026-02-07 10:05:33 +0100

    remove debug, small typo fix

--- plugins/theatres.py
+++ plugins/theatres.py
@@ -8,18 +8,13 @@ def getAllEventsIksoris(url):
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36'}  # Some sites require user-agent
         response = requests.get(url, headers=headers, verify=False)
         response.raise_for_status()  # Raise an error for bad status codes
-
         # Parse the HTML content
         soup = BeautifulSoup(response.text, 'html.parser')
-        
         # Find all <a> tags and extract href attributes
         events = []
         for h2_tag in soup.find_all('h2', ["h5","text-primary"]):
            events.append(h2_tag.get_text())
-        
-        print(events)
         return events
-
     except requests.exceptions.RequestException as e:
         print(f"Error fetching {url}: {e}")
         return []
@@ -31,18 +26,14 @@ def checkNewEvents():
     for theatre in theatres:
         eventsOnSite = []
         newEvents = []
-        
         if theatre['system'] == "iksoris":
             eventsOnSite = getAllEventsIksoris(theatre['url'])
-            print("iksoris")
         elif theatre['system'] == "roma":
             print("ROMA")
         elif theatre['system'] == "xxx":
             print("xxx")
-
+            
         eventsInDb = db.retrieveEventsFromTheatre(theatre['name'])
-        
-        
         for event in eventsInDb:
             if event['name'] not in eventsOnSite:
                 db.removeEvent('name',event['name'])
