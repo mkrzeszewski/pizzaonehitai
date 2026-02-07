f5821e9ae8068a89be31e92fd27a81b17dbc9aba
mkrzeszewski <64884451+mkrzeszewski@users.noreply.github.com>
2026-02-07 03:16:52 +0100

    insert stocks to db

--- plugins/stocks.py
+++ plugins/stocks.py
@@ -1,17 +1,28 @@
 import plugins.pizzadatabase as db
 import plugins.ai as ai
+import json
 
 #we do it once per month
 def generateStocks():
-    db.removeAllStocks()
     users = db.retrieveAllUsers()
     userList = []
     for user in users:
         userList.append(user['name'])
     response = ai.generateStocks(userList)
+    json_response = response.strip().lstrip('```json\n').rstrip('```')
+    initiateStocksDB(json_response)
+
+    #debug
     print(str(response))
     return str(response)
 
+def initiateStocksDB(json_stocks):
+    db.removeAllStocks()
+    if json_stocks:
+        json_stocks = json.loads(json_stocks)
+        for stock in json_stocks:
+            db.insertStock(stock['name'], stock['symbol'], 100, 10000)
+            
 #we do it once per month
 def removeAllStocks():
     return ""
